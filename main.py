423d8d4621f40ad615fd3f3c27bc7b67d763fc30
Kos4ya <79753344+Kos4ya@users.noreply.github.com>
2021-03-16 19:17:48 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-from random import*
-f = open("BD/bd1.txt", "w")
-f.write("паггозвопщупкпщзткщпркпрурпгщкрпщгтпцпгцрпцрщпрыащцрпшгпрщрщПЩЫШЦКПЩоащгрпкпшгрпгрпщрпщпгщрпщыпигкиц")
-f.close()
-for i in range(20):
-    f = open("BD/"f"{i}.txt", "w")
+from random import *
+import os
+
+for i in range(int(input("количество файлов: "))):
+    f = open("BD/"f"{input('введите имя')}.txt", "w")
     f.write(str(randint(1, 1000)))
     f.close()
+
+for i in os.walk("BD"):
+    for j in i[2]:
+        f = open(f"BD/{j}", "r")
+        c = f.read()
+        print(c)
+        f.close()
+
+for i in os.walk("BD"):
+    for j in i[2]:
+        os.remove(f"BD/{j}")
